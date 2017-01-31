7c54cc72d234bc225c6eb9c455f4af391b4b149a
SpudNiche <nicholas.r.lajoie@gmail.com>
2017-01-30 21:32:54 -0500

    Finally figured out how to use stat(2)

--- HW2/prob4.c
+++ HW2/prob4.c
@@ -4,25 +4,34 @@
 // Description: Print total number of BLOCKS used by list of files passed on command line. 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[])
 {
-	int i, status; 
-	char *command[argc];  
+	int i, status, blocksum; 
+	char command[1000];
+    struct stat s; 
 
 	if (argc < 2) {
-		perror("No arguments passed!\n"); 
+		perror("No arguments passed.\n"); 
 		return -1; 
 	}
+    // Call each command line argument with 'stat' and parse data for # of blocks 
 	for (i = 1; i < argc; i++) {
-		command[i] = argv[i]; 
-		printf("%s\n",command[i]);
-		status = system(command[i]); 
-		if (status == -1) {
-			perror("System call failure.\n");
-			return -2;
-		}
+        //snprintf(command, sizeof(command), "stat %s", argv[i]); 
+		//printf("%s\n", command);
+        stat(argv[i], &s); 
+        printf("Stat blocksize: %lld\n", (long long) s.st_blocks); 
+        //status = system(command); 
+		//if (status == -1) {
+		//	perror("System call failure.\n");
+		//	return -2;
+		//}
+
 	}	
 	
 	return 0; 
