17488f31c6c5ba5785f39f410a8e392ae2201e4f
Nicholas Ross LaJoie <nicholas.r.lajoie@gmail.com>
2017-02-10 21:10:36 -0500

    Header file for prob 3 complete

--- HW4/prob3/x_strstr.h
+++ HW4/prob3/x_strstr.h
@@ -3,3 +3,10 @@
 // Date: February 10, 2017
 // File: x_strstr.h
 // Description: Header file for the x_strstr() function.
+
+#ifndef X_STRSTR_H
+#define X_STRSTR_H
+
+char *x_strstr(const char *haystack, const char *needle); 
+
+#endif
