c32666422f2580ef3db2e820db6831af75547720
Nicholas Ross LaJoie <nicholas.r.lajoie@gmail.com>
2017-02-13 20:45:01 -0500

    Problem 3 complete, started problem 1

--- HW4/prob3/prob3.c
+++ HW4/prob3/prob3.c
@@ -3,3 +3,39 @@
 // Date: February 10, 2017 
 // File: prob3.c 
 // Description: Testing implementation of the x_strstr() function.  
+// Sources: https://www.tutorialspoint.com/c_standard_library/c_function_strstr.htm
+
+#include <stdio.h> 
+#include "x_strstr.h"
+
+int main(int argc, char * argv[]) 
+{
+    const char *haystack;
+    const char *needle;
+    char *p;
+
+    // Receive command line arguments 
+    if (argc == 3) {
+        needle = argv[2];
+        haystack = argv[1];
+    } else if (argc == 2) {
+        needle = ""; 
+        haystack = argv[1];
+    } else if (argc == 1) {
+        needle = ""; 
+        haystack = "";
+    } else {
+        perror("Invalid number of arguments passed"); 
+        return 1;
+    }
+
+    // Process input 
+    p = x_strstr(haystack, needle);
+    if (p != NULL) {
+        printf("%s\n", p);
+    } else {
+        printf("Returned NULL\n");
+    }
+
+    return 0; 
+}
--- HW4/prob3/x_strstr.c
+++ HW4/prob3/x_strstr.c
@@ -3,3 +3,42 @@
 // Date: February 10, 2017
 // File: x_strstr.c
 // Description: Implementation of the x_strstr() function.
+
+#include <stdio.h>
+#include "x_strstr.h"
+
+char *x_strstr(const char *haystack, const char *needle)
+{
+    int i = 0, j = 0, nlen = 0, hlen = 0;
+ 
+    // If arguments are NULL or the haystack is empty, return NULL
+    if (haystack == NULL || needle == NULL) {
+        return NULL;
+    } else if (haystack[0] == '\0') {
+        return NULL;
+    } else if (needle[0] == '\0') {
+        return (char *) haystack; // Return entire haystack if needle is ""
+    }
+    // Get length of needle 
+    while (needle[nlen] != '\0') {
+        nlen++; 
+    }
+    // Get length of haystack 
+    while (haystack[hlen] != '\0') {
+        hlen++;
+    }
+    // Find first instance of needle within the haystack 
+    while (i < hlen) {
+        if (haystack[i] == needle[j]) {
+           if (j == nlen - 1) {
+               return (char *) &haystack[i - nlen + 1]; 
+           } else {
+               j++;
+           }
+        } else {
+            j = 0; 
+        }
+        i++;
+    }
+    return NULL; 
+}
