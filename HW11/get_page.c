333bd967e3cb6372d57b80ab41acb1bed6f7536e
Nicholas Ross LaJoie <nicholas.r.lajoie@gmail.com>
2017-04-19 11:38:06 -0500

    Halfway through hw11 lol

---
+++ HW11/get_page.c
@@ -0,0 +1,12 @@
+// Author: Nicholas LaJoie 
+// ECE 331 - Homework 11, Problem 1
+// April 18, 2017
+// C Program that fetches a webpage using a TCP Client
+// Source: 
+
+
+
+int main(int argc, char * argv[])
+{
+
+}
---
+++ HW11/get_page.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+print(urllib.request.urlopen("http://www.google.com").read())
